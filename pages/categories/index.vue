bae55d10f17a27715347163fa84b9f56cfe57226
cameronwickes <63425305+cameronwickes@users.noreply.github.com>
2020-04-11 00:51:05 +0100

    Update index.vue

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -2,7 +2,7 @@
   <div id="categories-page" class="page-wrapper categories-page">
     <site-hero 
     title="Categories" 
-    subtitle="View all topics below"
+    subtitle="Pick a topic to see relevant articles"
     image="/uploads/categoriesfinal.jpg" />
     <main-section theme="one-column">
       <categories-grid />
