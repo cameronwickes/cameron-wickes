2e967b6cd9a11c3d970b88b7f6db7cebd18d1497
cameronwickes <63425305+cameronwickes@users.noreply.github.com>
2020-04-10 13:36:47 +0100

    Update ContactForm.vue

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -4,7 +4,7 @@
     <div class="field">
       <label class="label is-medium">Name</label>
       <div class="control has-icons-left">
-        <input class="input is-medium" type="text" placeholder="Full Name" />
+        <input class="input is-small" type="text" placeholder="Full Name" />
         <span class="icon is-small is-left">
           <font-awesome-icon icon="user" />
         </span>
@@ -15,7 +15,7 @@
       <label class="label is-medium">Email</label>
       <div class="control has-icons-left">
         <input
-          class="input is-medium"
+          class="input is-small"
           type="email"
           placeholder="Email Address"
         />
@@ -29,7 +29,7 @@
       <label class="label is-medium">Subject</label>
       <div class="control has-icons-left">
         <input
-          class="input is-medium"
+          class="input is-small"
           type="text"
           placeholder="Summary"
         />
@@ -43,10 +43,13 @@
       <label class="label is-medium">Message</label>
       <div class="control">
         <textarea
-          class="textarea is-medium"
+          class="textarea is-small"
           placeholder="Detailed description of your comment, request, etc"
           rows="8"
         ></textarea>
+         <span class="icon is-small is-left">
+          <font-awesome-icon icon="comment" />
+        </span>
       </div>
     </div>
     <div class="control">
