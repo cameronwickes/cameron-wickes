24e60bbdba034e5c1867e2d830800b8420c7b5d4
cameronwickes <63425305+cameronwickes@users.noreply.github.com>
2020-04-10 15:50:10 +0100

    Update index.vue

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="categories-page" class="page-wrapper categories-page">
-    <site-hero title="Categories" image="https://cdn.pixabay.com/photo/2015/02/24/15/41/dog-647528__340.jpg" />
+    <site-hero title="Categories" image="https://cameron-wickes-blog.s3.eu-west-2.amazonaws.com/categories3.jpg" />
     <main-section theme="one-column">
       <categories-grid />
     </main-section>
