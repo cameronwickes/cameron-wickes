89d36977ec11e736ab06b2ddd61a050253811265
cameronwickes <63425305+cameronwickes@users.noreply.github.com>
2020-04-10 15:38:47 +0100

    Update index.vue

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="categories-page" class="page-wrapper categories-page">
-    <site-hero title="Categories" image="uploads/maarten-van-den-heuvel-8EzNkvLQosk-unsplash.jpg" />
+    <site-hero title="Categories" image="/uploads/maarten-van-den-heuvel-8EzNkvLQosk-unsplash.jpg" />
     <main-section theme="one-column">
       <categories-grid />
     </main-section>
