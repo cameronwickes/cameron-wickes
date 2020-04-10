536bbca4d35f1ea7cfe6d03b3db4f05f7277b6cd
cameronwickes <63425305+cameronwickes@users.noreply.github.com>
2020-04-11 00:56:51 +0100

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -8,7 +8,7 @@
     <main-section theme="sidebar-right">
       <template v-slot:default>
         <div class="content">
-          <h>Hi!<h>
+          <h1>Hi!<h1>
           <p>I am Cameron Wickes, a 17 year old student from Cumbria, with a love for Cybersecurity and all things Computer Science.<p>
           <p>I am interested in all sorts of areas, and am always trying to expand my knowledge of the subject. </p>
           <p>This blog serves as a space to share and collate my personal learning experiences as I progress in the field, in the hopes to provide a knowledge bank for up and coming enthusiasts.</p>
