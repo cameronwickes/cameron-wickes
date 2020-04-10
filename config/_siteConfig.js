4cd306f09d24dc453deea9738a98d4b68b260026
cameronwickes <63425305+cameronwickes@users.noreply.github.com>
2020-04-10 17:01:33 +0100

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -11,7 +11,7 @@ import theCMS from '../cms/netlify/cms'
 export default {
   siteName: 'Cameron Wickes',
   tagline: 'A student with a passion for cybersecurity.',
-  featureImage: '/uploads/checkmed.jpg',
+  featureImage: '/uploads/michael-dziedzic-aqyguywncsm-unsplash.jpg',
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
