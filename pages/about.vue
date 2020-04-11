53fe55c5a7e7f73ce73f2ea8b244779d41700d09
cameronwickes <63425305+cameronwickes@users.noreply.github.com>
2020-04-11 19:10:03 +0100

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -18,10 +18,13 @@
           <h3>Contact Me</h3>
           <p>If you'd like to get in touch with me, please do so via one of the sites below:</p>
           <font-awesome-icon :icon="{prefix: 'fab',iconName: 'twitter'}" size="lg" />
-          <a href="https://twitter.com/cameronwickes0" target="_blank">Twitter</a>
+          <a href="https://twitter.com/cameronwickes0" target="_blank"><font color="09b4ed">Twitter</font></a>
           <br>
           <font-awesome-icon :icon="{prefix: 'fab',iconName: 'linkedin'}" size="2x" />
-          <a href="https://www.linkedin.com/in/cameron-wickes-7b32aa192/" target="_blank">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/cameron-wickes-7b32aa192/" target="_blank"><font color="09b4ed">LinkedIn</font></a>
+          <br>
+          <font-awesome-icon icon="envelope" size="2x" />
+          <p>enquiries@cameronwickes.com<p>
         </div>
       </template>
 
