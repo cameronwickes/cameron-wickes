074cca42eb1fab143586cbe49a6d8bb449ca7c91
Cameron Wickes <63425305+cameronwickes@users.noreply.github.com>
2020-07-04 19:23:48 +0100

    Update freelance.vue

--- pages/freelance.vue
+++ pages/freelance.vue
@@ -9,14 +9,14 @@
       <template v-slot:default>
         <div class="content">
           <h3>Freelance Work</h3>
-          <p>Need help designing a website? Have an old computer that needs upgrading? You're in the right place. I offer a range of technical services including but not limited to:</p>
+          <p>Need help designing a website? Have an old computer that needs upgrading?</p>
+          <p>I offer a range of technical services including but not limited to:</p>
           <ul>
           <li>Graphic Design</li>
           <li>Website Development</li>
           <li>Computer Hardware Upgrades</li>
           </ul>
-          <p>Please get in touch to discuss specific requirements, and find out what I can do for you!</p>
-          <p>enquiries@cameronwickes.com</p>
+          <p>If you have a project that you'd like me to be a part of, want to see if I offer a service or want to discuss specific requirements, please get in touch at <span style='color:#09b4ed'>enquiries@cameronwickes.com</span>
           <p>Thank you!</p>
           <h3>Testimonials</h3>
      
