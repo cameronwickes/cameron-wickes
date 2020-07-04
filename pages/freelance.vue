5cc602e485b65ea87ef08a94cbd47a6e2a2e78d5
Cameron Wickes <63425305+cameronwickes@users.noreply.github.com>
2020-07-04 19:19:16 +0100

    Update freelance.vue

--- pages/freelance.vue
+++ pages/freelance.vue
@@ -8,18 +8,19 @@
     <main-section theme="sidebar-right">
       <template v-slot:default>
         <div class="content">
-          <h3>Hi!</h3>
-          <p>I’m Cameron Wickes, an 18-year-old student from Cumbria, with a love for computers and information security.</p>
-          <h3>Contact Me</h3>
-            <p>If you'd like to get in touch with me, please do so via one of the methods below:</p>
-            <font-awesome-icon :icon="{prefix: 'fab',iconName: 'twitter'}" size="3x" style="vertical-align:middle" />
-            <a style="font-size:20px; font-weight:bold; vertical-align:middle; color:#09b4ed" href="https://twitter.com/cameronjwickes" target="_blank">   Twitter</a>
-            <br>
-            <font-awesome-icon :icon="{prefix: 'fab',iconName: 'linkedin'}" size="3x" style="vertical-align:middle" />
-            <a style="font-size:20px; font-weight:bold; vertical-align:middle; color:#09b4ed" href="https://www.linkedin.com/in/cameron-wickes-7b32aa192/" target="_blank">    LinkedIn</a>
-            <br>
-            <font-awesome-icon icon="envelope" size="3x" style="vertical-align:middle"/>
-            <span style="font-size:20px; font-weight:bold; vertical-align:middle; color:#09b4ed">   enquiries@cameronwickes.com</span> 
+          <h3>Freelance Work</h3>
+          <p>Need help designing a website? Have an old computer that needs upgrading? You're in the right place. I offer a range of technical services including but not limited to:</p>
+          <ul>
+          <li>Graphic Design</li>
+          <li>Website Development</li>
+          <li>Computer Hardware Upgrades</li>
+          </ul>
+          <p>Please get in touch to discuss specific requirements, and find out what I can do for you!</p>
+          <p>enquiries@cameronwickes.com</p>
+          <p>Thank you!</p>
+          <h3>Testimonials</h3>
+     
+ 
         </div>
       </template>
 
