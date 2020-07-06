45da98a195d1ea208c7173b1c6073803c1c274de
Cameron Wickes <63425305+cameronwickes@users.noreply.github.com>
2020-07-06 23:57:09 +0100

    Update freelance.vue

--- pages/freelance.vue
+++ pages/freelance.vue
@@ -67,8 +67,8 @@
                 <svg width="46" height="39" viewBox="0 0 16 16" class="bi bi-file-earmark-text-fill" fill="white" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2 3a2 2 0 0 1 2-2h5.293a1 1 0 0 1 .707.293L13.707 5a1 1 0 0 1 .293.707V13a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V3zm7 2V2l4 4h-3a1 1 0 0 1-1-1zM4.5 8a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7zM4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z"/></svg>
               </div>
               <div class="full-width-testimonial-content">
-                <h5 class="full-width-testimonial-text"></h5>
-                <p class="full-width-testimonial-source">Kirkby Lonsdale Tourism</p>
+                <h5 class="full-width-testimonial-text">Created a series of graphics for the Kirkby Lonsdale Easter Trail.</h5>
+                <p class="full-width-testimonial-source">Kirkby Lonsdale CIC</p>
                 <span class="full-width-testimonial-source-context">Graphic Design</span>
               </div>
             </div>
